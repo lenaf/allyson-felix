a180ded3107f16e11d316a6d6fa6c4c254679bee
lena.forti <lena.forti@disneystreaming>
2025-05-02 13:17:24 -0400

    spacing

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -2,6 +2,6 @@
 
 export default function Divider() {
   return (
-    <div className="flex-grow w-full relative border-t border-gray-400 my-4"></div>
+    <div className="flex-grow w-full relative border-t border-gray-400 my-6"></div>
   );
 }
