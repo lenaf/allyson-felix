f7471b99c01f31c68de9c7b746da8d430baab487
lena.forti <lena.forti@disneystreaming>
2025-05-05 12:21:45 -0400

    laurel

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,11 @@
 
 import Section from "@/components/common/Section";
 import background from "public/images/medals_9:16.jpg"
+import laurel from "public/images/laurel.png"
+
 import BackgroundImage from "./common/BackgroundImage";
+
+import Image from "next/image"
 
 export default function Hero() {
   return (
@@ -11,6 +15,13 @@ export default function Hero() {
       <BackgroundImage
         alt={'Allyson Felix wearing her olympic medals'}
         src={background}
+      />
+      <Image
+        alt={'Tribeca Festival Official Selection'}
+        src={laurel}
+        placeholder="blur"
+        width={200}
+        className={`absolute top-5 left-5 w-20 md:top-10 md:left-10 md:w-36`}
       />
     </Section>
 
