409d313ab2201339f03ad2282d096c60ea18e9a9
lena.forti <lena.forti@disneystreaming>
2025-05-30 15:36:20 -0400

    tweak

--- src/components/Sales.tsx
+++ src/components/Sales.tsx
@@ -3,24 +3,15 @@
 import SectionHeader from "./common/SectionHeader";
 import Section from "./common/Section";
 
-import React from "react";
-
-
 export default function Sales() {
   return (
 
     <Section id='sales' className="prose px-4 sm:px-8 md:px-12 py-12" >
-
       <div >
         <SectionHeader>WORLDWIDE SALES</SectionHeader>
         <div className="text-white text-xl mb-2">The Film Sales Company </div>
-        <div className="">Andrew Herwitz, President        </div>
-
-
+        <div className="">Andrew Herwitz, President </div>
       </div>
-
-
-
     </Section>
 
   );
