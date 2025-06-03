26d68a3036108fa96d95af8ad13731561e7ae04a
lena.forti <lena.forti@disneystreaming>
2025-06-03 12:22:05 -0400

    logos

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,10 @@
 import Divider from '../common/Divider'
 import DCTV from "public/images/DCTV.png"
 import getToYes from "public/images/getToYesWhite.png"
-import together from "public/images/together.png"
+import together from "public/images/togethxr white logo.png"
+import harnish from "public/images/harnish.png"
+import artemis from "public/images/artemis.png"
+import mastone from "public/images/mastone.png"
 
 import Image from "next/image"
 
@@ -40,7 +43,10 @@ export default function Footer() {
         <div className="mb-2 flex p-4 justify-center items-center gap-8 space-x-2 text-sm">
           <a href='https://www.dctvny.org/s/' target="_blank"> <Image height={50} src={DCTV} alt='DCTV Logo' /></a>
           <a href='https://gettoyesproductions.com/' target="_blank"><Image height={50} src={getToYes} alt='Get to Yes Logo' /></a>
-          <a href='https://www.togethxr.com/' target="_blank"><Image height={50} width={125} src={together} alt='Get to Yes Logo' /></a>
+          <a href='https://www.togethxr.com/' target="_blank"><Image height={50} width={125} src={together} alt='Togethxr Logo' /></a>
+          <a href='https://artemisrising.org/' target="_blank"><Image height={50} width={125} src={artemis} alt='Artemis Rising Foundation Loho' /></a>
+          <a href='https://thehf.org/' target="_blank"><Image height={50} width={125} src={harnish} alt='The Harshisch Foundation Logo' /></a>
+          <a href='' target="_blank"><Image height={50} width={40} src={mastone} alt='Madstone Logo' /></a>
         </div>
 
         <div className="flex justify-center items-center text-sm">
