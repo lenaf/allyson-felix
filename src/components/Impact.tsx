4de6f31c5c4f6603ea0a45d40a34babb6646362d
lena.forti <lena.forti@disneystreaming>
2025-06-04 16:52:21 -0400

    updates

--- src/components/Impact.tsx
+++ src/components/Impact.tsx
@@ -50,14 +50,14 @@ export default function Impact() {
                   <a
                     target="_blank"
                     href={'https://www.thebirthfund.com/get-involved'}
-                    className="underline-offset-4">
+                  >
                     Start a birthFUNDing circle</a>
                 </div>
                 <div>
 
                   <a target="_blank"
                     href={'https://birthfund.donorsupport.co/page/FUNEVZZTEDN'}
-                    className="underline-offset-4"
+
                   >
                     Donate today, and help fund one birth</a>
                 </div>
@@ -69,9 +69,18 @@ export default function Impact() {
             title={'Robert Wood Johnson Foundation'}
             link={'https://www.rwjf.org/'}
             description={
-              <p>
-                RWJF is a leading national philanthropy dedicated to taking bold leaps to transform health in our lifetime by working to dismantle structural racism and other barriers to health. Through funding, convening, advocacy, and evidence-building, RWJF works side-by-side with communities, practitioners, and institutions to get to health equity faster and pave the way together to a future where health is no longer a privilege, but a right.`
-              </p>
+              <div>
+                <p className="my-1">
+                  RWJF is a leading national philanthropy dedicated to taking bold leaps to transform health in our lifetime by working to dismantle structural racism and other barriers to health. Through funding, convening, advocacy, and evidence-building, RWJF works side-by-side with communities, practitioners, and institutions to get to health equity faster and pave the way together to a future where health is no longer a privilege, but a right.`
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://www.rwjf.org/en/grants/active-funding-opportunities.html'}
+                  >
+                    Active Funding Opportunities</a>
+                </div>
+              </div>
             }
           />
 
@@ -79,18 +88,37 @@ export default function Impact() {
             title={'Black Mamas Matter Alliance'}
             link={'https://blackmamasmatter.org/'}
             description={
-              <p>
-                A Black women-led cross-sectoral alliance that centers Black mamas and birthing people to advocate, drive research, build power, and shift culture for Black maternal health, rights, and justice.
-              </p>}
+              <div>
+                <p className="my-1">
+                  A Black women-led cross-sectoral alliance that centers Black mamas and birthing people to advocate, drive research, build power, and shift culture for Black maternal health, rights, and justice.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://www.mightycause.com/organization/Black-Mamas-Matter-Alliance'}
+                  >
+                    Donate today</a>
+                </div>
+              </div>
+            }
           />
 
           <Resource
             title={'SisterSong'}
             link={'https://www.sistersong.net/'}
             description={
-              <p>
-                Our purpose is to build an effective network of individuals and organizations to improve institutional policies and systems that impact the reproductive lives of marginalized communities.
-              </p>
+              <div>
+                <p className="my-1">
+                  Our purpose is to build an effective network of individuals and organizations to improve institutional policies and systems that impact the reproductive lives of marginalized communities.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://host.nxt.blackbaud.com/donor-form/?envid=p-j9YabJh9Y0GTMn0jEjfP6w&formId=a415e417-77e8-466d-a2b1-92f72174039f&svcid=renxt&zone=usa'}
+                  >
+                    Donate directly to GA, KY, or NC state programming</a>
+                </div>
+              </div>
             }
           />
 
@@ -98,36 +126,74 @@ export default function Impact() {
             title={'Ancient Song Doula Services'}
             link={'https://www.ancientsongdoulaservices.com/'}
             description={
-              <p>
-                We provide doula training and services, offer community education, and advocate for policy change to support reproductive and birth justice.
-              </p>
+              <div>
+                <p className="my-1">
+                  We provide doula training and services, offer community education, and advocate for policy change to support reproductive and birth justice.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://www.paypal.com/donate/?business=ancientsongdoula%40gmail.com&cmd=_donations&currency_code=USD&item_name=Ancient%20Song%20Doula%20Services'}
+                  >
+                    Donate to support doula services</a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'Birth Center Equity'}
             link={'https://birthcenterequity.org/'}
             description={
-              <p>
-                We are creating a world where every community has access to a birth center and midwives, families, and communities of color thrive.
-              </p>
+              <div>
+                <p className="my-1">
+                  We are creating a world where every community has access to a birth center and midwives, families, and communities of color thrive.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://birthcenterequityfund.networkforgood.com/'}
+                  >
+                    Donate to ensure access to community birthing centers across the U.S.</a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'Ms. Foundation'}
             link={'https://forwomen.org/'}
             description={
-              <p>
-                We build women’s collective power in the U.S. by investing in, and strengthening, the capacity of women-led movements to advance meaningful social, cultural, and economic change.
-              </p>
+              <div>
+                <p className="my-1">
+                  We build women’s collective power in the U.S. by investing in, and strengthening, the capacity of women-led movements to advance meaningful social, cultural, and economic change.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://forwomen.org/get-involved/'}
+                  >
+                    Get Involved
+                  </a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'Commonsense Childbirth Inc.'}
             link={'https://commonsensechildbirth.org/'}
             description={
-              <p>
-                Our programs operate across the perinatal health ecosystem with a focus on safety, quality and workforce development in support of – and rooted in – our vision for equitable perinatal care.
-              </p>
+              <div>
+                <p className="my-1">
+                  Our programs operate across the perinatal health ecosystem with a focus on safety, quality and workforce development in support of – and rooted in – our vision for equitable perinatal care.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://commonsensechildbirth.org/donate/'}
+                  >
+                    Support clinical services, midwifery education, and perinatal workforce care
+                  </a>
+                </div>
+              </div>
             } />
 
         </div>
@@ -138,62 +204,117 @@ export default function Impact() {
             title={'Southern Birth Justice Network'}
             link={'https://southernbirthjustice.org/'}
             description={
-              <p>
-                Our model, Black Midwives Care, is designed to meet the needs of pregnant, birthing and postpartum Black mothers and parents.
-              </p>
+              <div>
+                <p className="my-1">
+                  Our model, Black Midwives Care, is designed to meet the needs of pregnant, birthing and postpartum Black mothers and parents.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://buy.stripe.com/fZe3cdcXP4bB99KcMN'}
+                  >
+                    Donate today
+                  </a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'HoPE Doula Program at Mount Sinai'}
             link={'https://www.hopedoulaprogram.org/'}
             description={
-              <p>
-                Mount Sinai Hospital, in partnership with NYC Health + Hospitals, the largest public hospital system in the nation, is piloting a community-based doula care program to serve low-income pregnant women.
-              </p>
+              <div>
+                <p className="my-1">
+                  The HOPE Program (Helping Promote Birth-Equity through Community-Based Doula Care) provides doula care, free of charge, to NYC residents who are pregnant or recently gave birth, regardless of immigration status.
+                </p>
+              </div>
             } />
 
           <Resource
             title={'Black Women’s Blueprint'}
             link={'https://restoreny.org/black-womens-blueprint/'}
             description={
-              <p>
-                Black Women’s Blueprint (B.W.B.) partners with mental health and obstetric providers to build racial equity in maternal health and early childhood services.
-              </p>
+              <div>
+                <p className="my-1">
+                  Black Women’s Blueprint (B.W.B.) partners with mental health and obstetric providers to build racial equity in maternal health and early childhood services.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://blackwomensblueprint.ourpowerbase.net/civicrm/contribute/transact?reset=1&id=4'}
+                  >
+                    Donate today
+                  </a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'One Brooklyn Health System'}
             link={'https://onebrooklynhealth.org/'}
             description={
-              <p>
-                One Brooklyn is scaling three evidence-based programs with the goal of reducing racial disparities in birth outcomes: Centering Pregnancy, Healthy Families, and Healthy Steps.
-              </p>
+              <div>
+                <p className="my-1">
+                  One Brooklyn is scaling three evidence-based programs with the goal of reducing racial disparities in birth outcomes: Centering Pregnancy, Healthy Families, and Healthy Steps.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://onebrooklynhealth.org/donate'}
+                  >
+                    Donate today
+                  </a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'CAMBA-Moving Mothers’ Health Forward'}
             link={'https://camba.org/programs/moving-mothers-health-forward-program/'}
             description={
-              <p>
-                Seeks to bridge the gap in access to care for underserved, high-risk women through on-site support services in our Brooklyn-based family shelters and NYCHA Developments served by CAMBA’s Cornerstone Community Centers.
-              </p>
+              <div>
+                <p className="my-1">
+                  Seeks to bridge the gap in access to care for underserved, high-risk women through on-site support services in our Brooklyn-based family shelters and NYCHA Developments served by CAMBA’s Cornerstone Community Centers.
+                </p>
+              </div>
             } />
 
           <Resource
             title={'Bonded By Baby | Social Creatures'}
             link={'https://www.thesocialcreatures.org/bondedbybaby'}
             description={
-              <p>
-                A comprehensive postpartum care program dedicated to supporting new parents for the crucial first year after childbirth through community-based cohorts.
-              </p>
+              <div>
+                <p className="my-1">
+                  A comprehensive postpartum care program dedicated to supporting new parents for the crucial first year after childbirth through community-based cohorts.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://onebrooklynhealth.org/donate'}
+                  >
+                    Donate to help sustain and grow our programs
+                  </a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'Oklahoma Birth Equity Initiative'}
             link={'https://www.okbirthequity.org/about?_gl=1*1ua0fs5*_up*MQ..*_ga*MTI5MDQ3NjM5NC4xNzQ4ODg1MDg2*_ga_J2PWH4WCP3*czE3NDg4OTUxNTMkbzIkZzEkdDE3NDg4OTUyMDckajYkbDAkaDA.'}
             description={
-              <p>
-                OKBEI works tirelessly to equip families to have healthy births with dignity and reduce health disparities among pregnant women—so that all of Oklahoma can grow together.              </p>
+              <div>
+                <p className="my-1">
+                  OKBEI works tirelessly to equip families to have healthy births with dignity and reduce health disparities among pregnant women—so that all of Oklahoma can grow together.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://onebrooklynhealth.org/donate'}
+                  >
+                    Support Oklahoma families in need with birthing care and doula training
+                  </a>
+                </div>
+              </div>
             } />
 
 
@@ -201,21 +322,62 @@ export default function Impact() {
             title={'The Birthing Place'}
             link={'https://thebirthingplace.co/'}
             description={
-              <p>
-                We are BIPOC-led, multilingual and serving our community with the desire to create a maternity care system that centers the marginalized.               </p>
+              <div>
+                <p className="my-1">
+                  We are BIPOC-led, multilingual and serving our community with the desire to create a maternity care system that centers the marginalized.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://thebirthingplace.co/support-our-growth/'}
+                  >
+                    $5M Campaign to Expand The Birthing Place in the Bronx
+                  </a>
+                </div>
+              </div>
             } />
 
           <Resource
             title={'Irth: Birth Without Bias'}
             link={'https://irthapp.com/'}
             description={
-              <p>
-                The number one “Yelp-like” platform for the pregnancy and new motherhood journey, made by and for people of color.              </p>
+              <div>
+                <p className="my-1">
+                  The number one “Yelp-like” platform for the pregnancy and new motherhood journey, made by and for people of color.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://irthapp.com/the-crown-circle/'}
+                  >
+                    Support Birth Equity with Irth
+                  </a>
+                </div>
+              </div>
+            } />
+
+          <Resource
+            title={'Columbia University Mailman School of Public Health'}
+            link={'https://www.publichealth.columbia.edu/'}
+            description={
+              <div>
+                <p className="my-1">
+                  Creating a community of scholars and partner institutions that contribute to the knowledge base and understanding of the determinants of mental health.
+                </p>
+                <div>
+                  <a
+                    target="_blank"
+                    href={'https://www.publichealth.columbia.edu/reproductive-health'}
+                  >
+                    Help support our research on maternal and reproductive health at Columbia Mailman
+                  </a>
+                </div>
+              </div>
             } />
         </div>
       </div>
 
-      <Divider className="my-6" />
+      {/* <Divider className="my-6" />
 
       <SectionSubHeader>An Important Conversation</SectionSubHeader>
 
@@ -226,7 +388,7 @@ export default function Impact() {
           In conjunction with Allyson’s story, the creative team behind She Runs The World hosted an important hour-long conversation on power, parenthood, and fixing the maternal health crisis.        </p>
         <p className="my-2">
           Tune in to hear Director and SiriusXM host Perri Peltz DrPH, in conversation with 11-time Olympic Medalist, Entrepreneur and Maternal Health Advocate Allyson Felix, birthFUND founder Elaine Welteroth, Executive Producer of She Runs the World and Peabody-award winning Director of Aftershock Tonya Lewis Lee, and Dr. Avenel Joseph, Vice President of Policy at the Robert Wood Johnson Foundation. Together, they delved into the maternal health crisis in America, the urgency of telling Allyson’s story, and role of midwifery and equity in future solutions. Listen to learn how you can engage in the future of maternal health in the United States.         </p>
-      </div>
+      </div> */}
 
     </Section>
 
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,6 +6,9 @@ import together from "public/images/togethxr white logo.png"
 import harnish from "public/images/harnish.png"
 import artemis from "public/images/artemis.png"
 import mastone from "public/images/mastone.png"
+import birthFUND from "public/images/birthFUND.png"
+import RWJF from "public/images/RWJF.png"
+
 
 import Image from "next/image"
 
@@ -42,11 +45,14 @@ export default function Footer() {
 
         <div className="mb-2 flex p-4 justify-center items-center gap-x-8 space-x-2 text-sm flex-wrap no-prose">
           <a href='https://www.dctvny.org/s/' target="_blank"> <Image className='my-2' height={50} src={DCTV} alt='DCTV Logo' /></a>
+          <a href='https://madstonecompany.com/' target="_blank"><Image className='my-2' height={50} width={40} src={mastone} alt='Madstone Logo' /></a>
           <a href='https://gettoyesproductions.com/' target="_blank"><Image className='my-2' height={50} src={getToYes} alt='Get to Yes Logo' /></a>
           <a href='https://www.togethxr.com/' target="_blank"><Image className='my-2' height={50} width={125} src={together} alt='Togethxr Logo' /></a>
           <a href='https://artemisrising.org/' target="_blank"><Image className='my-2' height={50} width={125} src={artemis} alt='Artemis Rising Foundation Loho' /></a>
           <a href='https://thehf.org/' target="_blank"><Image className='my-2' height={50} width={125} src={harnish} alt='The Harshisch Foundation Logo' /></a>
-          <a href='https://madstonecompany.com/' target="_blank"><Image className='my-2' height={50} width={40} src={mastone} alt='Madstone Logo' /></a>
+          <a href='https://www.thebirthfund.com/' target="_blank"><Image className='my-2' height={50} width={125} src={birthFUND} alt='BirthFund Logo' /></a>
+          <a href='https://www.rwjf.org/' target="_blank"><Image className='my-2' height={50} width={125} src={RWJF} alt='Robert Wood Johnson Foundation Logo' /></a>
+
         </div>
 
         <div className="flex justify-center items-center text-sm">
