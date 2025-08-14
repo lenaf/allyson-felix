3e5131bfe1777b5142b1a3790cf4675f26749475
lena.forti <lena.forti@disneystreaming>
2025-08-14 12:37:49 -0400

    style

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -16,8 +16,8 @@ const Quote = ({ quote, className }: { quote: IQuote, className?: string }) => {
   const { text, publication, author, publicationLogo } = quote;
   return (
     <div className={`xs:w-[336px] md:w-[600px] ${className}`}>
-      <div className="text-primary text-xl md:text-2xl  text-center uppercase font-black tracking-wider">{text}</div>
-      <div className="text-white text-md md:text-lg text-right mr-12 flex flex-grow-0 items-center justify-end">
+      <div className="text-white text-xl md:text-2xl  text-center uppercase font-black tracking-wider">{text}</div>
+      <div className="text-md md:text-lg text-right mr-12 flex flex-grow-0 items-center justify-end">
         {author && <span className={`${publication.length > 7 ? 'mr-2' : ''} flex no-wrap`}>{author},</span>}
         <Image
           className="w-24 max-h-6 m-0 object-contain"
