e3785e004eff1080aebb3966ba40a0c38c85663d
lena.forti <lena.forti@disneystreaming>
2025-08-20 15:22:00 -0400

    fix build

--- src/data/image.ts
+++ src/data/image.ts
@@ -1,7 +1,9 @@
 
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import { dataset, projectId } from "@/data/sanity";
+
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID!;
+const dataset = process.env.SANITY_STUDIO_DATASET!;
 
 // https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
