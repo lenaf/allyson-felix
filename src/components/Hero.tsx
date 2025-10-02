287f888b75dec8e3fb8666b6137f46b2dfbcf9d4
lena.forti <lena.forti@disneystreaming>
2025-10-01 20:11:44 -0400

    laurels from cms

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,61 +1,55 @@
-"use client"
+"use client";
 
 import Section from "@/components/common/Section";
-import background from "public/images/medals_9:16.jpg"
+import background from "public/images/medals_9:16.jpg";
 
 import BackgroundImage from "./common/BackgroundImage";
-import laurel from "public/images/laurel.png"
-import laurel2 from "public/images/BFF_Laurels2025_OFFICIAL SELECTION_W.png"
-import laurel3 from "public/images/Laurels The MVFF 2025 white.png"
-import Image from "next/image"
+import Image from "next/image";
 import { urlFor } from "@/data/image";
 
-export default function Hero({ laurels }: { laurels: { image: any, title: string }[] }) {
-  return (
+export default function Hero({
+  laurels,
+}: {
+  laurels: { image: any; title: string }[];
+}) {
+  const use2ColsOnEachSideForDesktop = laurels.length > 10;
+  const firstHalfLaurels = laurels.slice(0, Math.ceil(laurels.length / 2));
+  const secondHalfLaurels = laurels.slice(Math.ceil(laurels.length / 2));
 
-    <Section id='home' className='flex h-[80vw] md:h-[60vw] lg:h-[50vw] xl:h-[40vw] flex-col justify-top items-left gap-4' >
+  return (
+    <Section
+      id="home"
+      className="flex h-[80vw] md:h-[60vw] lg:h-[50vw] xl:h-[40vw] flex-col justify-top items-left gap-4"
+    >
       <BackgroundImage
-        alt={'Allyson Felix wearing her olympic medals'}
+        alt={"Allyson Felix wearing her olympic medals"}
         src={background}
       />
       <div className="p-4 md:p-8 relative">
-        <div className={`absolute relative`}>
-          {/* {laurels.map((l, i) =>
-            <Image
+        <div className="flex justify-between">
+          {[firstHalfLaurels, secondHalfLaurels].map((laurelsSet, i) => (
+            <div
               key={i}
-              alt={l.title}
-              src={urlFor(l.image)
-                .width(200)
-                .quality(80)
-                .auto("format")
-                .url()}
-              width={200}
-              height={200}
-              className={`w-20 sm:w-24 md:w-32 lg:w-36 mb-4 md:mb-8`}
-            />)} */}
-          <Image
-            alt={'Tribeca Festival Official Selection Laurel'}
-            src={laurel}
-            width={200}
-            className={`w-24 md:w-36 mb-8`}
-          />
-          <Image
-            alt={'BGG Official Selection Laurel'}
-            src={laurel2}
-            width={200}
-            className={`w-24 md:w-36 mb-8`}
-          />
-          <Image
-            alt={"The Martha's Vineyard Film Festival Laurel"}
-            src={laurel3}
-            width={200}
-            className={`w-24 md:w-36`}
-          />
+              className={`grid grid-cols-1 gap-1 sm:gap-2 md:gap-4 lg:gap-6 items-center ${use2ColsOnEachSideForDesktop ? "md:grid-cols-2" : ""} `}
+            >
+              {laurelsSet.map((l, j) => (
+                <Image
+                  key={`${i}-${j}`}
+                  alt={l.title}
+                  src={urlFor(l.image)
+                    .width(200)
+                    .quality(80)
+                    .auto("format")
+                    .url()}
+                  width={200}
+                  height={200}
+                  className={`w-16 h-auto sm:w-24 md:w-32 lg:w-36`}
+                />
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </Section>
-
   );
 }
-
-
